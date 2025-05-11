13ece1f800834073398d58434925e25edd811202
angelgap <longobardo2005@gmail.com>
2025-05-11 21:27:30 +0200

    CreacionSpringboootProyect

---
+++ Backend/Vee/src/main/java/com/S2DAW/Proyecto/Vee/Vee/VeeApplication.java
@@ -0,0 +1,13 @@
+package com.S2DAW.Proyecto.Vee.Vee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeeApplication.class, args);
+	}
+
+}
---
+++ Backend/Vee/src/test/java/com/S2DAW/Proyecto/Vee/Vee/VeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S2DAW.Proyecto.Vee.Vee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
